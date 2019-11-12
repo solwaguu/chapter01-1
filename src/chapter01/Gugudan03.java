a9d6520d18e79520ebf074550b9c2aa415dc2192
Ahn Dae Huyck <kickscar@gmail.com>
2019-11-12 19:00:00 +0900

    pretty gugudan

--- src/chapter01/Gugudan03.java
+++ src/chapter01/Gugudan03.java
@@ -2,5 +2,12 @@ package chapter01;
 
 public class Gugudan03 {
 	public static void main(String[] args) {
+		for(int i = 1; i <= 9; i++){
+			for(int j = 1; j <= 9; j++){
+				int result = i * j;
+				System.out.print(j + " X " + i + " = " + result + "\t");
+			}
+			System.out.println("===========================");
+		}		
 	}
 }
